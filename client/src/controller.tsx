63fa25349fa61d12258e7a93e07e6533b69d220b
fmsyt <fms20yt@gmail.com>
2023-06-04 23:08:45 +0900

    :+1: reconnect on websocket is closed

--- client/src/controller.tsx
+++ client/src/controller.tsx
@@ -3,7 +3,7 @@ import { action } from "./interface";
 import AppButton from "./components/button";
 import AppSlider from "./components/slider";
 
-import { Box, Grid, Pagination, Paper, Stack, styled } from "@mui/material";
+import { Box, CircularProgress, Grid, Pagination, Paper, Stack, styled } from "@mui/material";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -21,53 +21,54 @@ const Controller = () => {
 
   const [webSocket, setWebSocket] = useState<WebSocket>();
 
-  const connectWebSocket = useCallback(() => {
-
-    const webSocket = new WebSocket(`ws://${window.location.hostname}:8000/ws`);
-
-    webSocket.addEventListener("open", (e: Event) => {
-      console.log("WebSocket Connection Established.");
-      setWebSocket(webSocket);
-    })
-
-    webSocket.addEventListener("close", (e: CloseEvent) => {
-      console.log("WebSocket Connection Closed.");
-      setWebSocket(undefined);
-    })
-
-    webSocket.addEventListener("message", (e: MessageEvent) => {
-
-      const obj = JSON.parse(e.data);
-
-      console.log('Message from server ', obj);
-
-      switch (obj?.method) {
-        default: break;
-
-        case "update_sheets":
-          setActions(obj?.data || []);
-          break;
-      }
-
-    })
-
-    return webSocket;
-
-  }, []);
-
   useEffect(() => {
 
-    const reConnect = () => {
-      !webSocket && connectWebSocket();
+    const connectWebSocket = () => {
+
+      if (webSocket) {
+        return;
+      }
+
+      console.info("Connecting WebSocket...");
+
+      const ws = new WebSocket(`ws://${window.location.hostname}:8000/ws`);
+
+      ws.addEventListener("open", (e: Event) => {
+        console.log("WebSocket Connection Established.");
+      })
+
+      ws.addEventListener("close", (e: CloseEvent) => {
+        console.log("WebSocket Connection Closed.");
+        setWebSocket(undefined);
+      })
+
+      ws.addEventListener("message", (e: MessageEvent) => {
+
+        const obj = JSON.parse(e.data);
+
+        console.log('Message from server ', obj);
+
+        switch (obj?.method) {
+          default: break;
+
+          case "update_sheets":
+            setActions(obj?.data || []);
+            break;
+        }
+
+      })
+
+      setWebSocket(ws);
     }
 
-    setTimeout(reConnect, 1000);
+    const timeout = setTimeout(connectWebSocket, 1000);
 
     return () => {
+      clearTimeout(timeout);
       webSocket && webSocket.close();
     }
 
-  }, [webSocket, connectWebSocket]);
+  }, [webSocket]);
 
 
   const putElement = useCallback((action: action, key?: React.Key) => {
@@ -88,24 +89,35 @@ const Controller = () => {
     <Box sx={{ padding: 8 }}>
       <Box sx={{ width: '100%' }}>
         <Stack direction="column" alignItems="center" spacing={1}>
-          <Grid container rowSpacing={1} columnSpacing={2}>
-            {actions.length > 0 && actions[page - 1].map((action, i) => (
-              <Grid key={i} item xs={12 / 4}>
-                <Item>
-                  {putElement(action)}
-                </Item>
-              </Grid>
-            ))}
-          </Grid>
 
-          {actions.length > 1 && (
-            <Pagination
-              count={actions.length}
-              color="primary"
-              onChange={(e, page) => setPage(page)}
-              page={page}
-              />
-          )}
+          {
+            webSocket?.readyState === WebSocket.OPEN
+            ? (
+              <>
+                <Grid container rowSpacing={1} columnSpacing={2}>
+                  {actions.length > 0 && actions[page - 1].map((action, i) => (
+                    <Grid key={i} item xs={12 / 4}>
+                      <Item>
+                        {putElement(action)}
+                      </Item>
+                    </Grid>
+                  ))}
+                </Grid>
+
+                {actions.length > 1 && (
+                  <Pagination
+                    count={actions.length}
+                    color="primary"
+                    onChange={(e, page) => setPage(page)}
+                    page={page}
+                    />
+                )}
+              </>
+            ): (
+              <CircularProgress />
+            )
+          }
+
         </Stack>
       </Box>
     </Box>
