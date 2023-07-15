b6de12eee532019975185d3058c8dacbb7b8938a
fmsyt <fms20yt@gmail.com>
2023-07-15 23:52:53 +0900

    :coffee: remove unused

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react"
+import React, { useMemo } from "react"
 import Board from "./Board"
 
 import { Route, Routes, BrowserRouter } from "react-router-dom";
