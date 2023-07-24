c1047bca787063d1e8153ab6a5a594561d6aa54c
fmsyt <fms20yt@gmail.com>
2023-07-25 00:14:59 +0900

    :+1: slider style applied

--- client/src/components/Slider.tsx
+++ client/src/components/Slider.tsx
@@ -4,6 +4,48 @@ import { controlProps } from "../interface";
 import { Slider as MuiSlider, Stack } from "@mui/material";
 import { emit } from "../functions";
 import { Events } from "../enum";
+import styled from "@emotion/styled";
+
+const sliderSize = 32;
+
+const BoldSlider = styled(MuiSlider)(({ theme }) => ({
+
+  "& .MuiSlider-thumb": {
+    width: 4,
+    height: sliderSize,
+    backgroundColor: "white",
+    borderRadius: 2,
+    cursor: "pointer",
+
+    "&:hover": {
+      boxShadow: "0 0 0 8px rgba(0, 0, 0, 0.1)",
+    },
+  },
+  "&.MuiSlider-vertical .MuiSlider-thumb": {
+    width: sliderSize,
+    height: 4,
+  },
+  "& .MuiSlider-rail": {
+    width: "100%",
+    height: sliderSize,
+    cursor: "pointer",
+    borderRadius: 2,
+    backgroundColor: "secondary.main",
+  },
+  "&.MuiSlider-vertical .MuiSlider-rail": {
+    width: sliderSize,
+    height: "100%",
+  },
+  "& .MuiSlider-track": {
+    width: "100%",
+    // height: 4,
+    height: sliderSize,
+    cursor: "pointer",
+    borderRadius: 2,
+    backgroundColor: "secondary.main",
+  }
+}));
+
 
 const Slider = (props: { ws: WebSocket, controlProps: controlProps, disabled?: boolean }) => {
 
@@ -34,10 +76,12 @@ const Slider = (props: { ws: WebSocket, controlProps: controlProps, disabled?: b
   return (
     <Stack spacing={2} direction="row" sx={{ mb: 1, width: "100%" }} alignItems="center">
       {/* <VolumeDown /> */}
-      <MuiSlider
+      <BoldSlider
         aria-label="Volume"
         defaultValue={Number(controlProps.current || 0)}
         disabled={disabled}
+        // orientation="vertical"
+        // sx={{ height: 300 }}
         { ...(controlProps.props || {}) }
         { ...events }
         />
