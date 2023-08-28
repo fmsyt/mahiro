3eda1cb6f17b0577d7726c1a5860643291580e7e
fmsyt <fms20yt@gmail.com>
2023-08-28 23:42:27 +0900

    :+1: split on platform

--- client/src/pages/tauri.tsx
+++ client/src/pages/tauri.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import App from '../App';
 
 const root = ReactDOM.createRoot(
@@ -9,6 +8,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App disableDrawer={false} />
+    <App disableDrawer={true} />
   </React.StrictMode>
 );
---
+++ client/vite.tauri.config.ts
@@ -0,0 +1,26 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite'
+
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    outDir: 'tauri.dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'tauri.html'),
+        config: resolve(__dirname, 'config.html'),
+      }
+    },
+    emptyOutDir: true
+  },
+  plugins: [
+    react(),
+  ],
+  clearScreen: false,
+  server: {
+    port: 1421,
+    strictPort: true,
+  }
+})
