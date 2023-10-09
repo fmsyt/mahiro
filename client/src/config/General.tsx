e74d0f65da3b2495a6153abe61de776f1b39443b
fmsyt <fms20yt@gmail.com>
2023-10-09 14:04:10 +0900

    :memo: memo

--- client/src/config/General.tsx
+++ client/src/config/General.tsx
@@ -1,9 +1,21 @@
 import { Stack, Typography } from "@mui/material";
 
-export default function General() {
+/**
+ * General tab for the config app.
+ * - application settings
+ *   - set connection to server
+ *   - OTP settings
+ * - visibility of the app
+ *   - switch dark mode
+ *   - set most top
+ *
+ * @returns { JSX.Element }
+ */
+export default function General(): JSX.Element {
   return (
     <Stack>
       <Typography variant="h5">General</Typography>
+
     </Stack>
   );
 }
