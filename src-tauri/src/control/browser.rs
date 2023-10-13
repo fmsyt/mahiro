f4277620edabca42c47e6bca662e3d3e294d4809
fmsyt <fms20yt@gmail.com>
2023-10-13 10:31:23 +0900

    :rocket: compile target by OS

--- src-tauri/src/control/browser.rs
+++ src-tauri/src/control/browser.rs
@@ -1,21 +1,18 @@
-use std::{env, process::{Command, Child}, io};
+use std::{process::{Command, Child}, io};
 
 pub fn browse(url: &String) -> Result<Child, io::Error> {
-    let command_result = match env::consts::OS {
-        "windows" => {
-            Command::new("cmd.exe").args(["/c", "start", url]).spawn()
-        }
-        "macos" => {
-            Command::new("open").args([url]).spawn()
-        }
-        "linux" => {
-            Command::new("xdg-open").args([url]).spawn()
-        }
-        _ => {
-            eprintln!("Error: Unsupported platform: {}", env::consts::OS);
-            Err(io::Error::new(io::ErrorKind::Other, "Unsupported platform"))
-        }
-    };
+
+    #[cfg(target_os = "windows")]
+    let command_result = Command::new("cmd.exe").args(["/c", "start", url]).spawn();
+
+    #[cfg(target_os = "macos")]
+    let command_result = Command::new("open").args([url]).spawn();
+
+    #[cfg(target_os = "linux")]
+    let command_result = Command::new("xdg-open").args([url]).spawn();
+
+    #[cfg(not(any(target_os = "windows", target_os = "macos", target_os = "linux")))]
+    let command_result = Err(io::Error::new(io::ErrorKind::Other, "Unsupported OS"));
 
     command_result
 }
