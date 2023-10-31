a650d8f5b828d332750e9d3be08aff728a56af85
motsuni <motsuni@yotta-rc.dev>
2023-10-31 21:58:22 +0900

    :bug: static files

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'static',
+    outDir: 'src-tauri/static',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
